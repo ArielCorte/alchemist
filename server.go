03eab28e464b44e1fa77b179f3610bb1e04affda
Ariel Corte <cortearielformal@gmail.com>
2024-01-21 18:45:19 -0300

    remove sql no needed for now, add ingredients

--- server.go
+++ server.go
@@ -1,22 +1,35 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-
 	"github.com/labstack/echo/v4"
-	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-
-	db, err := sql.Open("sqlite3", "./alchemist.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
 	e := echo.New()
 	e.File("/", "views/index.html")
+
+	ingredients := []Ingredient{
+		{"air", true},
+		{"earth", true},
+	}
+
+	e.GET("/unlocked_ingredients", func(c echo.Context) error {
+
+		html := ""
+
+		for _, ingredient := range ingredients {
+			if ingredient.unlocked {
+				html += "<li>" + ingredient.name + "</li>"
+			}
+		}
+
+		return c.HTML(200, html)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+type Ingredient struct {
+	name     string
+	unlocked bool
 }
