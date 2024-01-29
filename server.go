6e20e988c317fefa28c68686d1769533f9602a3f
Ariel Corte <cortearielformal@gmail.com>
2024-01-29 17:07:30 -0300

    restructure ingredients, sort ingredient map by name value

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"sort"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,36 +12,37 @@ func main() {
 	e := echo.New()
 	e.File("/", "views/index.html")
 
-	ingredients := map[string]Ingredient{
-		"air":      {"air", true, [][]string{}},
-		"earth":    {"earth", true, [][]string{}},
-		"fire":     {"fire", true, [][]string{}},
-		"water":    {"water", true, [][]string{}},
-		"rain":     {"rain", false, [][]string{{"air", "water"}, {"cloud", "water"}}},
-		"pressure": {"pressure", false, [][]string{{"air", "air"}}},
-		"steam":    {"steam", false, [][]string{{"water", "fire"}, {"energy", "water"}}},
-		"cloud":    {"cloud", false, [][]string{{"air", "steam"}}},
+	ingredients := map[int]Ingredient{
+		1: {1, "air", [][]int{}, []string{}},
+		2: {2, "earth", [][]int{}, []string{}},
+		3: {3, "fire", [][]int{}, []string{}},
+		4: {4, "water", [][]int{}, []string{}},
+		5: {5, "rain", [][]int{{1, 4}, {8, 4}}, []string{}},
+		6: {6, "pressure", [][]int{{1, 1}}, []string{}},
+		7: {7, "steam", [][]int{{4, 3}, {9, 4}}, []string{}},
+		8: {8, "cloud", [][]int{{1, 7}}, []string{}},
 	}
 
-	soup_ing := []string{}
+	unlocked_ingredients := []int{}
 
+	soup_ing := []int{}
 	e.Static("/css", "css")
 
 	e.GET("/unlocked_ingredients", func(c echo.Context) error {
 
 		html := ""
 
-		sorted_ingredients := make([]string, 0, len(ingredients))
+		sorted_ingredients := make(PairList, 0, len(ingredients))
 
-		for ingredient := range ingredients {
-			sorted_ingredients = append(sorted_ingredients, ingredient)
+		for id, ingredient := range ingredients {
+			sorted_ingredients = append(sorted_ingredients, Pair{id, ingredient.Name})
 		}
-		sort.Strings(sorted_ingredients)
+		sort.Sort(sorted_ingredients)
 
 		for _, i := range sorted_ingredients {
-			ingredient := ingredients[i]
-			if ingredient.unlocked {
-				html += "<li hx-target='#current-ingredients' hx-post='/add_ingredient?ingredient=" + ingredient.name + "'>" + ingredient.name + "</li>"
+			ingredient := ingredients[i.Key]
+			if containi(unlocked_ingredients, ingredient.Id) {
+				html += "<li hx-target='#current-ingredients' hx-post='/add_ingredient?ingredient=" + ingredient.Name + "'>" + ingredient.Name + "</li>"
 			}
 		}
 
@@ -48,9 +50,14 @@ func main() {
 	})
 
 	e.POST("/add_ingredient", func(c echo.Context) error {
-		ingredient := c.QueryParam("ingredient")
+		string_id := c.QueryParam("ingredient")
+		//convert string to int
+		id, err := strconv.Atoi(string_id)
+		if err != nil {
+			return c.String(200, err.Error())
+		}
 
-		soup_ing = append(soup_ing, ingredient)
+		soup_ing = append(soup_ing, id)
 
 		if len(soup_ing) >= 2 {
 			// send request to craft soup
@@ -67,7 +74,7 @@ func main() {
 			return c.String(200, err.Error())
 		}
 		crafted_ing := ingredients[crafted_ing_name]
-		crafted_ing.unlocked = true
+		unlocked_ingredients = append(unlocked_ingredients, crafted_ing.Id)
 		ingredients[crafted_ing_name] = crafted_ing
 		return c.HTML(200, "<span hx-get='unlocked_ingredients' hx-target='#unlocked-ingredients' hx-trigger='load'>"+crafted_ing_name+"</span>")
 	})
@@ -79,20 +86,20 @@ func main() {
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func craftSoup(ings map[string]Ingredient, soup_ing []string) (string, error) {
+func craftSoup(ings map[string]Ingredient, soup_ing []int) (string, error) {
 	if len(soup_ing) < 2 {
 		return "", errors.New("not enough ingredients")
 	}
 	// check if ingredients are valid
 	for _, ingredient := range ings {
-		for _, recipe := range ingredient.ingredients {
+		for _, recipe := range ingredient.Parents {
 			if soup_ing[0] != soup_ing[1] {
 				if contains(recipe, soup_ing[0]) && contains(recipe, soup_ing[1]) {
-					return ingredient.name, nil
+					return ingredient.Name, nil
 				}
 			} else {
 				if len(recipe) == 2 && recipe[0] == soup_ing[0] && recipe[1] == soup_ing[1] {
-					return ingredient.name, nil
+					return ingredient.Name, nil
 				}
 			}
 		}
@@ -110,8 +117,29 @@ func contains(s []string, e string) bool {
 	return false
 }
 
-type Ingredient struct {
-	name        string
-	unlocked    bool
-	ingredients [][]string
+func containi(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
+
+type Ingredient struct {
+	Id      int
+	Name    string
+	Parents [][]int
+	Tags    []string
+}
+
+type Pair struct {
+	Key   int
+	Value string
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
